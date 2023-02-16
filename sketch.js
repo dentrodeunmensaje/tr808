960d7cb83943d9178e499aabcbbc27afec1e8b21
Rodrigo Villarreal <rodrigovillarreal@Rodrigos-MacBook-Pro.local>
2023-02-15 19:12:19 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  // put setup code here
+  createCanvas(400,400);
+
+
+}
+
+function draw() {
+  // put drawing code here
+  background(100,255,0);
+  fill(255,0,0);
+  ellipse(100,100,100,100);
+
+}
